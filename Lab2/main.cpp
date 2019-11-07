b73e4111c1fb3257ac720a1aacdfe5738545f7fc
Vincent Gagnon <vincentgagnon53@gmail.com>
2019-11-07 10:51:08 -0500

    Ajout variables globals

---
+++ Lab2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int ressourcesAlloue[5][5];
+int ressourcesMaxDemandees[5][5];
+int ressourcesTotal[5];
+
+int main() {
+
+    cout << "Bonsoir";
+
+    getchar();
+
+    return 0;
+}
